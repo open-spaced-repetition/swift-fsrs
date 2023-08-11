eb4e41f587d68bba6ef3febabc62e09f7273254e
Ben Smiley-Andrews <ben@sdk.chat>
2023-08-11 09:02:01 +0200

    Initial Commit

---
+++ FSRS/FSRSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FSRSApp.swift
+//  FSRS
+//
+//  Created by Ben on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct FSRSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
