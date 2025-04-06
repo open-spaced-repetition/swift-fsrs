313433346d6d2a49e478e4e28aac038eff64fd9a
ningkaiqiang <52522174+ningkaiqiang@users.noreply.github.com>
2025-04-06 12:49:28 +0900

    fix: defaultW can't be changed

--- Sources/FSRS/Models/FSRSDefaults.swift
+++ Sources/FSRS/Models/FSRSDefaults.swift
@@ -33,7 +33,7 @@ public class FSRSDefaults {
 
     var defaultRequestRetention = 0.9
     var defaultMaximumInterval = 36500.0
-    var defaultW = [
+    let defaultW = [
         0.40255, 1.18385, 3.173, 15.69105, 7.1949,
         0.5345, 1.4604, 0.0046, 1.54575, 0.1192,
         1.01925, 1.9395, 0.11, 0.29605, 2.2698,
